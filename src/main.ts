4c69476ba1070863a93957cb761946b7f1aef2a3
Daniel Freire <dgz1606@gmail.com>
2026-05-18 18:44:18 -0500

    chore(scene): change object and camera properties

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import "./style.css";
 import * as THREE from "three/webgpu";
 
 // Canvas
@@ -5,12 +6,28 @@ const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 
 // Scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x1a1a1a);
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+// Position
+mesh.position.set(0.7, -0.6, 1);
+
+// Scale
+mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+mesh.rotation.reorder("YXZ");
+mesh.rotation.x = Math.PI / 4;
+mesh.rotation.y = Math.PI / 4;
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(1.5);
+scene.add(axesHelper);
 
 // Sizes
 const sizes = {
@@ -22,6 +39,8 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
+
+camera.lookAt(mesh.position);
 
 // Renderer
 const renderer = new THREE.WebGPURenderer({ canvas, antialias: true });
